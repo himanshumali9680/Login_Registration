34e428188c2525fdd92dbfb3b22e9f1acb04029b
himanshumali9680 <himanshumali9680@gmail.com>
2024-02-27 22:41:22 +0530

    resetUI and register comp

--- client/src/components/Reset.js
+++ client/src/components/Reset.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import avatar from "../assets/profile.png";
+import styles from "../styles/Username.module.css";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import { resetPassowrdValidation } from "../helper/validate";
 
 const Reset = () => {
-  return (
-    <div>Reset</div>
-  )
-}
+  const formik = useFormik({
+    initialValues: {
+      password: 'admin@123',
+      confirm_pwd: 'admin@123',
+    },
+    validate: resetPassowrdValidation,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
 
-export default Reset
+  return (
+    <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass} style={{width: "50%"}}>
+          <div className="title flex flex-col items-center">
+            <div className="text-5xl font-bold"> Reset</div>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              Enter new passowrd.
+            </span>
+          </div>
+          <form className="py-20" onSubmit={formik.handleSubmit}>
+            <div className="textbox flex flex-col items-center gap-6">
+              <input
+                {...formik.getFieldProps("password")}
+                className={styles.textbox}
+                type="text"
+                placeholder="New Password"
+              />
+              <input
+                {...formik.getFieldProps("confirm_pwd")}
+                className={styles.textbox}
+                type="text"
+                placeholder="Repeat Passowrd"
+              />
+              <button className={styles.btn} type="submit">
+                 Reset
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reset;
--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -9,6 +9,52 @@ export async function usernameValidate(values){
     return errors;
 }
 
+/**validate password */
+export async function passwordValidate(values){
+    const errors = passwordVerify({}, values);
+
+    return errors;
+}
+
+/**Validate reser passowrd */
+export async function resetPassowrdValidation(values){
+    const errors = passwordVerify({}, values);
+
+    if(values.password !== values.confirm_pwd){
+        errors.exist = toast.error("Password not match...!")
+    }
+
+    return errors;
+}
+
+/**validate register form */
+export async function registerValidation(values){
+    const errors = usernameVerify({}, values);
+    passwordVerify(errors, values);
+    emailVerify(errors, values);
+
+    return errors;
+}
+
+/********************************************* */
+
+/**validate passowrd */
+function passwordVerify(errors = {}, values){
+    const specialChars = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+
+    if(!values.password){
+        errors.password = toast.error("Password Required...!");
+    } else if(values.password.includes(" ")){
+        errors.password = toast.error("Wrong Password...!")
+    }else if(values.password.length < 4){
+        errors.password = toast.error("Password must be more than 4 charcters long");
+    }else if(!specialChars.test(values.password)){
+        errors.password = toast.error("Password must have special character");
+        
+    }
+    return errors;
+}
+
 /**validate username */
 
 function usernameVerify(error = {}, values){
@@ -16,6 +62,18 @@ function usernameVerify(error = {}, values){
         error.username = toast.error('Username Required...!')
     }else if(values.username.includes(" ")){
         error.username = toast.error('Invalid Username...!')
+    }
+    return error;
+}
+
+/**validate email */
+function emailVerify(error ={}, values){
+    if(!values.email){
+        error.email = toast.error("Email Required...!");
+    }else if(values.email.includes(" ")){
+        error.email = toast.error("Wrong email...!")
+    }else if(/^(?=.*[0-9])(?=.*[!#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(values.email)){
+        error.email = toast.error("Invalid email address...!")
     }
     return error;
 }
